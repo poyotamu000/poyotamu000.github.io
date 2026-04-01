890fb76be0a45fa87b4219dc94e0d03573e4bd2c
poyotamu000 <49392029+poyotamu000@users.noreply.github.com>
2026-04-01 03:42:38 +0000

    Deploying to gh-pages from @ poyotamu000/poyotamu000.github.io@c6ae96261985abfb2db5ba73f0cefcfdc29a79d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%69%6B%69@%6A%73%6B.%69%6D%69.%69.%75-%74%6F%6B%79%6F.%61%63.%6A%70", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/poyotamu000", "_blank");
         },
       },{
         id: 'social-rss',
@@ -533,13 +533,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
