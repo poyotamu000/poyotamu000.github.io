f0b7c5eeac7de9110cc0e070361d46499d59d882
poyotamu000 <49392029+poyotamu000@users.noreply.github.com>
2026-04-02 00:51:20 +0000

    Deploying to gh-pages from @ poyotamu000/poyotamu000.github.io@c3f88ce3385b28d1d10aa4cf461417b475e56ff7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-collaborator",
+          title: "Collaborator",
+          description: "collaborators and research partners",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
